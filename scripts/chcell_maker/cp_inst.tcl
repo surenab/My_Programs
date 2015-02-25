04e641b68cd578a10e8867c93596ed5c0d8875c4
Suren Abrahamyan <suren.abrahamyan89@gmail.com>
2015-02-25 23:03:58 +0400

    Revert "all"
    This reverts commit 654b02b609dc67eeece69e026cc0056a3a7519b2.

--- scripts/chcell_maker/cp_inst.tcl
+++
@@ -1,3 +0,0 @@
-dm::copyCells [dm::getCells edge4 -libName new] -libDestName new2  -cellDestName xt
-set cell_dell [oa::DesignOpen new2 edge4 layout readOnly]
-le::flatten [de::getFigures {{-4 -4} {100 100 }} -design $cell_dell] -levels 10
